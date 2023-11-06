4e86cb96248f2d29708efdb12306a7658969f5fb
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-06 16:38:17 +0100

    zad1

---
+++ lab5/src/pl/people/zad1/Person.java
@@ -0,0 +1,14 @@
+package pl.people.zad1;
+
+class Person {
+    private static int counter;
+    private String name;
+
+    public Person(){
+        counter++;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+}
