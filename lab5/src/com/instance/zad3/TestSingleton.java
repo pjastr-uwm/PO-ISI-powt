db9b48ddbb6f9ca655e53e4412b141a0ddc912d2
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-06 16:54:38 +0100

    zad3

---
+++ lab5/src/com/instance/zad3/TestSingleton.java
@@ -0,0 +1,9 @@
+package com.instance.zad3;
+
+public class TestSingleton {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+}
