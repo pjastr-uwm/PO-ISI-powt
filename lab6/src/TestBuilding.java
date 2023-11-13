2e0feaeef348526a2e31793893415cf04485e70b
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-13 16:48:48 +0100

    zad11

---
+++ lab6/src/TestBuilding.java
@@ -0,0 +1,11 @@
+import pl.edu.uwm.zad11.Building;
+import pl.edu.uwm.zad11.House;
+
+public class TestBuilding {
+
+    public static void main(String[] args) {
+        Building b1 = new Building(100, "white");
+        House h1 = new House(50, "red", 10);
+
+    }
+}
---
+++ lab6/src/pl/edu/uwm/zad11/Building.java
@@ -0,0 +1,15 @@
+package pl.edu.uwm.zad11;
+
+public class Building {
+
+    private int height;
+    private String color;
+
+    public Building() {
+    }
+
+    public Building(int height, String color) {
+        this.height = height;
+        this.color = color;
+    }
+}
